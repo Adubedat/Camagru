1e9ab46b9cf72a9f1a28fd06ae72a19d97b56d17
Arthur Dubedat <arthur.dubedat@gmail.com>
2017-06-21 21:32:06 +0200

    push

---
+++ header/header.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="header/header.css">
+    <link href="https://fonts.googleapis.com/css?family=Arizonia" rel="stylesheet">
+  </head>
+  <body>
+    <div id="header-container">
+        <div class="header-div" id="accueil">Camagru</div>
+        <div id="left-bloc">
+          <div class="header-div" id="Gallery"><p>Gallery</p></div>
+
+          <div class="header-div" id="signup"><p>Sign up</p></div>
+          <?php
+            session_start();
+            if (isset($_SESSION[logged_in_user]) && $_SESSION[logged_in_user] == "yes") {
+          ?>
+          <div class="header-div" id="logout"><p>Log out</p></div>
+          <?php
+            } else {
+          ?>
+          <div class="header-div" id="signin"><p>Login</p></div>
+          <?php }; ?>
+        </div>
+    </div>
+  </body>
+</html>
