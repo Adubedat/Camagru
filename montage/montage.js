85f476234fadabcf5ae64ef93c5b9789752aef6d
Arthur Dubedat <arthur.dubedat@gmail.com>
2017-08-30 18:59:34 +0200

    drag zone + move images

--- montage/montage.js
+++ montage/montage.js
@@ -4,6 +4,7 @@ window.addEventListener("load", function(){
   var canvas = document.getElementById('canvas');
   var photo = document.getElementById('photo');
   var take_snap = document.getElementById('take-snap');
+  var moving = false;
   var localstream;
 
   navigator.getMedia = ( navigator.getUserMedia ||
@@ -13,6 +14,9 @@ window.addEventListener("load", function(){
 
   start_camera();
   function start_camera() {
+    while (document.getElementsByClassName('draggable-images')[0] != null) {
+      remove_draggable_images();
+    }
     if (document.getElementById('uploaded_img') != null) {
       document.getElementById('uploaded_img').remove();
     }
@@ -46,17 +50,19 @@ window.addEventListener("load", function(){
   }
 
   function takepicture() {
-    canvas.width = 640;
-    canvas.height = 480;
     if (document.getElementById('uploaded_img') != null) {
-      canvas.getContext('2d').drawImage(document.getElementById('uploaded_img'), 0, 0);
+      var uploaded_img = document.getElementById('uploaded_img');
+      canvas.getContext('2d').drawImage(uploaded_img, 0, 0, uploaded_img.naturalWidth, uploaded_img.naturalHeight, 0, 0, 640, 480);
     }
     else {
+      canvas.width = 640;
+      canvas.height = 480;
       canvas.getContext('2d').drawImage(video, 0, 0);
     }
     var data = canvas.toDataURL('image/png');
     photo.setAttribute('src', data);
-    photo.style.width = '320px';
+    photo.style.maxWidth = '320px';
+    photo.style.maxHeight = '240px';
   }
 
   take_snap.addEventListener('click', function(ev){
@@ -68,6 +74,9 @@ window.addEventListener("load", function(){
   document.getElementById('enable-camera').addEventListener('click', start_camera);
 
   function stop_camera() {
+    while (document.getElementsByClassName('draggable-images')[0] != null) {
+      remove_draggable_images();
+    }
     var video = document.getElementById('video');
     video.pause();
     video.src = "";
@@ -102,18 +111,79 @@ window.addEventListener("load", function(){
 
   function display_uploaded_image(file) {
     var reader = new FileReader();
-    var node = document.getElementById('montage');
+    var node = document.getElementById('drag-zone');
 
     reader.addEventListener('load', function() {
 
       var img_element = document.createElement('img');
       img_element.setAttribute('id', 'uploaded_img');
       img_element.src = this.result;
+      img_element.onload = function() {
+      }
       node.insertBefore(img_element, node.firstChild);
       document.getElementById('upload-img').style.display = 'none';
 
     });
 
     reader.readAsDataURL(file);
+  }
+
+  var images_list = document.getElementsByClassName('list-images');
+  for (var i = 0; i < images_list.length; i++) {
+    images_list[i].addEventListener('click', image_to_drag_zone);
+  }
+
+  function image_to_drag_zone() {
+    if (document.getElementById('uploaded_img') != null || document.getElementById('video').paused != true) {
+      var src = this.getAttribute('src');
+      var new_img = document.createElement('img');
+      var id = this.getAttribute('id');
+      if (document.getElementById(id + ' drag') != null) {
+        document.getElementById(id + ' drag').remove();
+      }
+      else {
+        var node = document.getElementById('drag-zone');
+        new_img.setAttribute('src', src);
+        new_img.setAttribute('class', 'draggable-images');
+        new_img.setAttribute('id', id + ' drag');
+        new_img.addEventListener('mousedown', initial_click);
+        node.insertBefore(new_img, node.firstChild);
+      }
+    }
+    else {
+      alert('You have to enable your webcam or upload a picture to do that.');
+    }
+  }
+
+  function initial_click(e) {
+    if(moving){
+      document.removeEventListener("mousemove", move);
+      moving = !moving;
+      return;
+    }
+
+    moving = !moving;
+    image = this;
+    var img_position = image.getBoundingClientRect();
+    Xdiff = e.clientX - img_position.left;
+    Ydiff = e.clientY - img_position.top;
+
+    document.addEventListener("mousemove", move, false);
+    document.Xdiff = Xdiff;
+    document.Ydiff = Ydiff;
+  }
+
+  function move(e) {
+    var newX = e.clientX - Xdiff;
+    var newY = e.clientY - Ydiff;
+
+    image.style.left = newX + 'px';
+    image.style.top = newY + 'px';
+  }
+  function remove_draggable_images() {
+    var images_list = document.getElementsByClassName('draggable-images');
+    for (var i = 0; i < images_list.length; i++) {
+      images_list[i].remove();
+    }
   }
 });
